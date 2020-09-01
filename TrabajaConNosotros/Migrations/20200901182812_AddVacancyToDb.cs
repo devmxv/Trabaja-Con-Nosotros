9db677c6bc963208e9a88e4957f3cc0643875152
devmxv <martin@devmxv.me>
2020-09-01 13:35:01 -0500

    Db and vacancy table setup. Fix to connection in appsettings

---
+++ TrabajaConNosotros/Migrations/20200901182812_AddVacancyToDb.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrabajaConNosotros.Migrations
+{
+    public partial class AddVacancyToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vacancy",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(nullable: false),
+                    Category = table.Column<int>(nullable: false),
+                    Salary = table.Column<float>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    Comments = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vacancy", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vacancy");
+        }
+    }
+}
